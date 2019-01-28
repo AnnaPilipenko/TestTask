08dd80b6a51ea30ba1373c70636b7593c3fda768
anna_pilipenko <hanna.pilipenko@gmail.com>
2019-01-28 15:51:40 +0200

    update db manager

--- TestTask/Helpers/DbManager.cs
+++ TestTask/Helpers/DbManager.cs
@@ -18,6 +18,19 @@ namespace TestTask.Helpers
         #region Users
 
         /// <summary>
+        /// Create a new user
+        /// </summary>
+        /// <param name="db">database context</param>
+        /// <param name="user">new user</param>
+        /// <returns>task (result contains new user Id or default(long))</returns>
+        public static async Task<long> CreateUser(this UserContext db, User user)
+        {
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+            return user.Id;
+        }
+
+        /// <summary>
         /// Search for user
         /// </summary>
         /// <param name="db">database context</param>
@@ -25,7 +38,8 @@ namespace TestTask.Helpers
         /// <returns>task (result contains user or default(User))</returns>
         public static async Task<User> GetUser(this UserContext db, long id)
         {
-            return await db.Users.FirstOrDefaultAsync(user => user.Id == id);
+            IQueryable<User> users = db.Users;
+            return await users.FirstOrDefaultAsync(user => user.Id == id);
         }
 
         /// <summary>
@@ -36,7 +50,8 @@ namespace TestTask.Helpers
         /// <returns>task (result contains user or default(User))</returns>
         public static async Task<User> GetUserAsync(this UserContext db, string emailOrSkypeLogin)
         {
-             return await db.Users.FirstOrDefaultAsync(user => user.Email.Equals(emailOrSkypeLogin, StringComparison.InvariantCultureIgnoreCase) ||
+            IQueryable<User> users = db.Users;
+            return await users.FirstOrDefaultAsync(user => user.Email.Equals(emailOrSkypeLogin, StringComparison.InvariantCultureIgnoreCase) ||
                                                user.SkypeLogin.Equals(emailOrSkypeLogin, StringComparison.InvariantCultureIgnoreCase));
         }
 
@@ -46,12 +61,13 @@ namespace TestTask.Helpers
         /// <param name="db">database context</param>
         /// <param name="lastId">last id returned in the previous query, is used when we have limit and scrolling to get the next batch of data</param>
         /// <param name="searchString">user's name, email or skype login, search as substring of chars, case insensitive</param>
-        /// <param name="roles">comma separated enumeration of user roles (empty means any), e.g.: User, Manager, Support, Admin</param>
+        /// <param name="roles">comma separated enumeration of user roles (empty means any), e.g.: User, Manager, Support, Admin (see TestTask.Models.UserRolesEnum)</param>
         /// <param name="isDisabled">search disabled users or not (null means any)</param>
         /// <param name="limit">maximum returned amount of records in one query</param>
         /// <returns>task (result contains list of users)</returns>
         public static async Task<List<User>> SearchUsersAsync(this UserContext db, long? lastId, string searchString, string roles, bool? isDisabled, int? limit)
         {
+            IQueryable<User> users = db.Users;
             if (!limit.HasValue || limit.Value <= 0)
             {
                 limit = DefaultLimit;
@@ -69,7 +85,7 @@ namespace TestTask.Helpers
                 }
             }
 
-            var users = (from user in db.Users
+            var result = (from user in users
                          where (lastId.HasValue ? user.Id > lastId : true) &&
                                (user.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase) || 
                                 user.Email.Contains(searchString, StringComparison.OrdinalIgnoreCase) ||
@@ -78,7 +94,7 @@ namespace TestTask.Helpers
                                (isDisabled.HasValue ? user.IsDisabled.Equals(isDisabled) : true)
                          select user).Take((int)limit);
 
-            return await users.ToListAsync();
+            return await result.ToListAsync();
         }
         #endregion
     }
